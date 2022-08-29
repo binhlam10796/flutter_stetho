ca89b6e6f4bbac53d7e868662d68a6265c0ec16a
binhlam10796 <42068261+binhlam10796@users.noreply.github.com>
2022-08-29 15:27:01 +0700

    commit override connectionFactory and keyLog

--- lib/src/http_client.dart
+++ lib/src/http_client.dart
@@ -72,6 +72,16 @@ class StethoHttpClient implements HttpClient {
   }
 
   @override
+  set connectionFactory(Future<ConnectionTask<Socket>> Function(Uri url, String? proxyHost, int? proxyPort)? f) {
+    client!.connectionFactory = f;
+  }
+
+  @override
+  set keyLog(Function(String line)? callback) {
+    client!.keyLog = callback;
+  }
+
+  @override
   Future<HttpClientRequest> get(String host, int port, String path) =>
       open("get", host, port, path);
 
